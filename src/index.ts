49e8ef92d569defdb2441ff6333b1a154a597487
Stepa Zdrazil <stopka.zdrazil@gmail.com>
2022-06-11 20:33:37 +0200

    Done demo

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
+import User from './model/User';
 
-const server = fastify()
+const server = fastify();
 
-server.get('/ping', async (request, reply) => {
-    return 'pong\n'
-})
+server.get('/ping', async () => (new User('hi')).name);
 
-server.listen({ port: 8000 }, (err, address) => {
-    if (err) {
-        console.error(err)
-        process.exit(1)
-    }
-    console.log(`Server listening ${address}`)
-})
+server.listen({ port: 8080, host: '0.0.0.0' }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening ${address}`);
+});
